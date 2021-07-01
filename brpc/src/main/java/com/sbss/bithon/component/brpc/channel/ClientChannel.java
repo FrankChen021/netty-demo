4fe3a8f330e90478437c0ccac87b04edf023ce80
frank chen <frank.chen021@outlook.com>
2021-07-01 18:46:02 +0800

    Update License

--- brpc/src/main/java/com/sbss/bithon/component/brpc/channel/ClientChannel.java
+++ brpc/src/main/java/com/sbss/bithon/component/brpc/channel/ClientChannel.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2020 bithon.cn
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.sbss.bithon.component.brpc.channel;
 
 import com.sbss.bithon.component.brpc.IService;
@@ -119,8 +135,9 @@ public class ClientChannel implements IChannelWriter, Closeable {
     }
 
     private void doConnect(int maxRetry) {
+        EndPoint endpoint = null;
         for (int i = 0; i < maxRetry; i++) {
-            EndPoint endpoint = endPointProvider.getEndpoint();
+            endpoint = endPointProvider.getEndpoint();
             try {
                 Future<?> connectFuture = bootstrap.connect(endpoint.getHost(), endpoint.getPort());
                 connectFuture.await(200, TimeUnit.MILLISECONDS);
@@ -140,7 +157,7 @@ public class ClientChannel implements IChannelWriter, Closeable {
                 throw new ServiceClientException("Unable to connect to server, interrupted");
             }
         }
-        throw new ServiceClientException("Unable to connect to server");
+        throw new ServiceClientException("Unable to connect to server({}:{})", endpoint.getHost(), endpoint.getPort());
     }
 
     public <T extends IService> ClientChannel bindService(Class<T> serviceType, T serviceImpl) {
